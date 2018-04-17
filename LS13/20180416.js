9ded1be3e3d10345a424d8073a50457f028d0e9e
lixiaofan1997 <773515092@qq.com>
2018-04-17 09:52:45 +0800

    JS对象

---
+++ LS13/20180416.js
@@ -0,0 +1,238 @@
+//闭包
+function counter(){
+    var n=0;
+    return{
+        count:function(){return  ++n;},
+        reset:function(){n=0;return n;}
+    }
+}
+var c=counter();
+var d=counter();
+console.log(c.count());
+console.log(d.count());
+console.log(c.reset());
+console.log(c.count());
+console.log(d.count());
+
+//new new Function
+console.log((new (new Function())) instanceof Function);
+//new Function实例化了一个函数对象，函数对象可以当做构造函数使用，所以再new一次产生了一个对象，类型object
+console.log((new (new Function())) instanceof Object);
+
+//JS对象综述
+//JS对象
+var obj = {
+    num:10,
+    str:"Hi",
+    show:function(){
+        console.log(this.str);
+    }
+};
+console.log(obj.num);//10
+console.log(obj.str);//Hi
+obj.show();			 //Hi
+
+//内置对象、宿主对象、自定义对象 
+//标准内置对象分为两类：1.构造函数对象 2.非构造器对象
+
+//Part 1
+var i = new String("str");          // String Object
+var h = new Number(1);              // Number Object
+var g = new Boolean(true);          // Boolean Object
+var j = new Object({name : "Tom"}); // Object Object
+var k = new Array([1, 2, 3, 4]);    // Array Object
+var l = new Date();                 // Date Object
+var m = new Error();
+var n = new Function();
+var o = new RegExp("\\d");
+
+// typeof
+console.log(typeof Array);
+console.log(typeof Function);
+console.log(typeof Date);
+console.log(typeof Number);
+console.log(typeof String);
+console.log(typeof Boolean);
+console.log(typeof Math);//和上边一样么？为什么
+console.log(typeof JSON);
+
+// Part 2
+// instanceof
+console.log(Object instanceof Function);
+console.log(Object instanceof Object);
+console.log(Boolean instanceof Function);
+console.log(Boolean instanceof Object);
+console.log(String instanceof Function);
+console.log(String instanceof Object);
+console.log(Number instanceof Function);
+console.log(Number instanceof Object);
+console.log(Function instanceof Function);
+console.log(Function instanceof Object);
+console.log(Array instanceof Function);
+console.log(Array instanceof Object);
+console.log(Date instanceof Function);
+console.log(Date instanceof Object);
+console.log(Math instanceof Function);
+console.log(Math instanceof Object);
+console.log(JSON instanceof Function);
+console.log(JSON instanceof Object);
+
+
+//访问器属性  注意：访问和设置时不加括号
+// 访问器属性 实例一
+var o = {
+    _x:1.0,//如果都写成x会怎样
+    get x(){
+        return this._x;//如果都写成x会怎样
+    },
+    set x(val){
+        this._x = val;//如果都写成x会怎样
+    }
+};
+console.log(o.x);//1
+o.x = 2;//实际上是调用了set方法
+console.log(o.x,o._x);//2 2
+
+
+//访问器属性 实例二 只读
+var o = {
+    _x:1.0,
+    get x(){
+        return this._x;
+    }
+};
+console.log(o.x);//1
+o.x = 2;
+console.log(o.x,o._x);//输出多少？
+
+// 访问器属性 实例三
+var p1 = {
+    _name:"Jack",
+    _age:23,
+    set age(val){
+        if(val>0&&val<150){
+            this._age = val;
+        }else{
+            console.log("请设置正常年龄");
+        }
+    },
+    get age(){
+        return this._age;
+    }
+};
+p1.age = 178;
+console.log(p1.age);
+
+//访问器属性 综合实例
+var p ={
+    x:1,
+    y:1,
+    get r(){return Math.sqrt(this.x*this.x+this.y*this.y);},
+    set r(newValue){
+        var oldValue = Math.sqrt(this.x*this.x+this.y*this.y);
+        var ratio = newValue/oldValue;
+        this.x*=ratio;
+        this.y*=ratio;
+    },
+    get theta(){return Math.atan2(this.y,this.x);}
+};
+var q = Object.create(p);
+q.x = 2;
+q.y = 2;
+console.log(q.r);
+console.log(q.theta);
+
+//创建对象 三种方法
+//通过字面量的方式创建 JS对象
+var obj = {
+    num:10,
+    str:"Hi",
+    show:function(){
+        return this.str;
+    }
+};
+console.log(obj.num);
+console.log(obj.str);
+console.log(obj.show());
+console.log(obj.__proto__);
+console.log(obj.__proto__ === Object.prototype);
+
+//通过Object工场方法创建JS对象,注：JS对象是通过原型链的方式实现的对象继承
+var newObj = Object.create(obj);//newObj的原型是obj
+newObj.age = 23;
+console.log(newObj.num);
+console.log(newObj.str);
+console.log(newObj.show());
+console.log(newObj.age);//自有属性
+console.log(newObj.__proto__);
+console.log(newObj.__proto__ === obj);
+
+//构造函数的方式创建JS对象  此处略讲，详情参照后续面向对象编程 注：JS对象是通过原型链的方式实现的对象继承
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.sayName = function(){
+    console.log("hello,i'm",this.name,this.age,"years old");
+};
+var person1 = new Person("Mike",21);
+person1.sayName();
+
+//注意：
+var objStr = new Object("xxx");
+console.log(typeof objStr);//object
+console.log(objStr instanceof String);//true
+
+var objNum = new Object(23);
+console.log(typeof objNum);//object
+console.log(objNum instanceof Number);//true
+
+var objBoolean = new Object(true);
+console.log(typeof objBoolean);//object
+console.log(objBoolean instanceof Boolean);//true
+
+
+var obj = {};
+obj.x = 2;//直接添加属性
+console.log(obj.x);//通过.访问属性
+obj.x = 5;//设置属性
+console.log(obj["x"]);//通过[]访问属性
+delete obj.x;//删除属性
+console.log(obj.x);
+
+//访问属性的for循环练习
+var obj2 = {
+    id_1:2,
+    id_2:4,
+    id_3:6,
+    id_4:8,
+    id_5:10
+};
+
+//思考obj3 和 obj4 内容是什么？为什么？
+var obj3 = {};
+for(var i=0;i<10;i++){
+    obj3.i = i;
+}
+
+var obj4 = {};
+for(var i=0;i<10;i++){
+    obj4[i] = i;
+}
+
+//遍历
+var obj={
+    x1:3,
+    x2:4,
+    x3:5,
+};
+for(var i=1;i<4;i++)
+{
+    console.log(obj["x"+i]);
+}
+
+
+
+
+
+
