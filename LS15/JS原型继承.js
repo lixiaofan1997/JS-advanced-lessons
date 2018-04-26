94ce3c105d6b74847c7189d60ddb48d3e4a73fda
lixiaofan1997 <773515092@qq.com>
2018-04-26 19:50:45 +0800

    JS原型继承

---
+++ LS15/JS原型继承.js
@@ -0,0 +1,174 @@
+//生成对象的3种方式：字面量直接生成、Object工场方法、构造函数实例化对象
+var obj1,obj2,obj3;
+//obj1通过字面量方式创建，并创建自身属性x
+//obj2通过Object静态方法创建，obj2的原型为obj1，并创建自身属性y
+//obj3通过构造函数来创建，构造函数为Obj3
+
+//通过字面量的方式创建 JS对象
+var obj = {
+    num:10,
+    str:"Hi",
+    show:function(){
+        return this.str;
+    }
+};
+console.log(obj.num);
+console.log(obj.str);
+console.log(obj.show());
+
+//通过Object工场方法创建JS对象,注：JS对象是通过原型链的方式实现的对象继承
+var newObj = Object.create(obj);
+newObj.age = 23;
+console.log(newObj.num);
+console.log(newObj.str);
+console.log(newObj.show());
+console.log(newObj.age);//自有属性
+
+var empty = {};
+var obj2 = Object.create(empty,{
+   x:{value:1}, y:{value:2,enumerable:true}
+});
+console.log(obj2);
+console.log(obj2.hasOwnProperty("x"));
+
+//构造函数的方式创建JS对象  此处略讲，详情参照后续面向对象编程 注：JS对象是通过原型链的方式实现的对象继承
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.sayName = function(){
+    console.log("hello,i'm",this.name,this.age,"years old");
+};
+
+var person1 = new Person("Mike",21);
+person1.sayName();
+var obj = {
+    num:10,
+    str:"Hi",
+    show:function(){
+        return this.str;
+    }
+};
+//console.log(obj.__proto__);
+console.log(obj.__proto__ === Object.prototype);
+
+var newObj = Object.create(obj);
+var newObj2 = Object.create(obj);//思考：多个对象同一个原型的情况
+newObj.age = 23;
+
+console.log(newObj.__proto__ === obj);
+console.log(newObj2.__proto__ === obj);
+
+//JavaScript的继承方式 是对象-对象的继承，对象要实现继承首先要有原型对象
+console.log(newObj.__proto__.__proto__);//Object.prototype
+console.log(newObj.__proto__.__proto__.__proto__);//null
+
+var proObj = {
+    z:3
+};
+
+var obj = Object.create(proObj);
+obj.x = 1;
+obj.y = 2;
+
+console.log(obj.x);//1
+console.log(obj.y);//2
+console.log(obj.z);//3
+
+console.log("z" in obj);//true
+console.log(obj.hasOwnProperty("z"));//false
+
+///////////Part2 原型链属性操作////////////
+obj.z = 5;
+
+console.log(obj.hasOwnProperty("z"));
+console.log(obj.z);
+console.log(proObj.z);
+
+obj.z = 8;
+console.log(obj.z);
+
+delete obj.z;//true
+console.log(obj.z);
+
+delete obj.z;//true
+console.log(obj.z);//still 3
+
+//如何删除原型上的属性
+delete  obj.__proto__.z;//或者delete proObj.z;
+console.log(obj.z);//此时彻底没有z了
+
+//注意：hasOwnProperty是原型方法
+//调用的主体为obj,先在自身上找该方法，找不到的话去原型链上去找
+//区别与Object.keys(obj)这种静态方法
+
+
+
+function Person(age,name) {
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.sayHi = function () {
+    console.log("Hi,i'm "+this.name);
+};
+var p1 = new Person(20,"Jack");
+console.log(p1.name);
+console.log(p1.age);
+p1.sayHi();
+
+function Person(name) {
+    this.name = name;
+    this.age = 21;
+}
+Person.prototype.sayHi = function () {
+    console.log("Hi,i'm ",this.name,this.age,"years old!");
+};
+
+var p1 = new Person("Mike");
+console.log(p1.name);
+console.log(p1.age);
+p1.sayHi();
+//name和age属性是定义在p1上了，还是定义在p1的原型上了
+//sayHi方法是定义在p1上了，还是定义在p1的原型上了
+//分析一下访问和调用的过程是怎样的
+
+
+//分析：属性和方法定义在构造函数中和写在prototype上这两种情况有什么不同?
+//没有私有属性情况下，常将方法添加到构造函数的prototype属性上，实现方法共享
+//而属性根据情况来确定是定义在构造函数中,还是定义在构造函数的prototype（即实例化对象的原型上）属性上
+
+console.log(p1.__proto__ === Person.prototype);
+
+function MyObj() { }
+MyObj.prototype.z = 3;
+
+var obj = new MyObj();
+obj.x = 1;
+obj.y = 2;
+
+console.log(obj.x);//1
+console.log(obj.y);//2
+console.log(obj.z);//3
+
+console.log("z" in obj);//true
+console.log(obj.hasOwnProperty("z"));//false
+
+///////////Part2 原型链属性操作////////////
+obj.z = 5;
+
+obj.hasOwnProperty("z");
+console.log(obj.z);
+console.log(MyObj.prototype.z);
+
+obj.z = 8;
+console.log(obj.z);
+
+delete obj.z;//true
+console.log(obj.z);
+
+delete obj.z;//true
+console.log(obj.z);//still 3
+
+//如何删除原型上的属性
+delete  obj.__proto__.z;//或者delete MyObj.prototype.z;
+console.log(obj.z);//此时彻底没有z了
