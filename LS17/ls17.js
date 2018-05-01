8985c46ec4dc2c27b4bc81ef1785de41c395dfd2
lixiaofan1997 <773515092@qq.com>
2018-05-01 09:09:52 +0800

    深入理解JS继承方式

---
+++ LS17/ls17.js
@@ -0,0 +1,234 @@
+//深入理解JS继承方式
+
+
+//通过Object.create静态方法创建的对象的原型共享问题
+var superObj = {
+    x:1,
+    y:2
+};
+var subObj_First = Object.create(superObj);
+var subObj_Second = Object.create(superObj);
+subObj_First.__proto__.x = 5;//若此行写为subObj_First.x = 5;结果又是如何？
+console.log(subObj_Second.x);
+
+//通过构造函数创建的对象的原型共享问题
+//以下写法实现了原型继承，但存在原型共享的问题
+//思考，name,age,id属性分别是添加到了那个对象上，这样写有什么问题？
+//如何给每个Studnent对象添加自有的name属性
+function Person(name){
+    this.name = name;
+}
+Person.prototype.age = 22;
+Person.prototype.showName = function(){console.log(this.name);};
+function Student(id){
+    this.id = id;
+}
+//var p1 = new Person("Mike");Student.prototype = p1;
+Student.prototype = new Person("Mike");
+var s1 = new Student(2017001);
+var s2 = new Student(2017002);
+
+//测试如下代码，思考为什么，这样的继承有什么弊端
+console.log(s1.name,s1.age,s1.id);
+console.log(s2.name,s2.age,s2.id);
+s1.__proto__.name = "Jack";
+console.log(s2.name);
+s2.__proto__.__proto__.age = 99;
+console.log(s2.age);
+
+// console.log(s1);
+// console.log(s1.__proto__);
+// console.log(s1.__proto__.__proto__);
+
+//如何给每个student对象单独添加自身属性name和age，内存是否浪费
+s1.name = "Bill";
+s1.age = 22;
+s2.name = "Colin";
+s2.age = 23;
+
+
+/*
+ //通过Object.create创建的对象的原型共享问题
+ //以下写法实现了原型继承，但存在原型共享的问题
+ //思考，name,age,id属性分别是添加到了那个对象上，这样写有什么问题？
+ function Person(name){
+ this.name = name;
+ }
+ Person.prototype.age = 22;
+ Person.prototype.showName = function(){console.log(this.name);};
+
+ var person1 = new Person("Mike");
+ var student1 = Object.create(person1,{id:{value:123,writable:true}});
+ var student2 = Object.create(person1,{id:{value:456,writable:true,enumerable:true}});
+
+ //测试如下代码，思考为什么，这样的继承有什么弊端
+ console.log(student1.name,student1.age,student1.id);
+ console.log(student2.name,student2.age,student2.id);
+ student1.__proto__.name = "Jack";
+ console.log(student2.name);
+ student2.__proto__.__proto__.age = 99;
+ console.log(student2.age);
+ */
+
+ //JS实现继承的形式 一
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.showName = function(){console.log(this.name);};
+function Student(name,age,id){
+    Person.call(this,name,age);
+    this.id = id;
+}
+Student.prototype.__proto__ = Person.prototype;
+var s1 = new Student("xxx",22,2017001);
+var s2 = new Student("www",23,2017002);
+
+
+//JS实现继承的形式 二
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.showName = function(){
+    console.log(this.name);
+};
+function Student(name,age,id){
+    Person.call(this,name,age);
+    this.id = id;
+}
+Student.prototype = Object.create(Person.prototype);
+// console.log(Person.prototype.constructor); //
+// console.log(Student.prototype.constructor); //
+Student.prototype.constructor = Student;
+var s1 = new Student("xxx",22,2017001);
+var s2 = new Student("www",23,2017002);
+
+
+//静态方法实例与原型方法实例
+var BaseClass = function() {};
+BaseClass.prototype.f2 = function () {
+    console.log("This is a prototype method ");
+};
+BaseClass.f1 = function(){//定义静态方法
+    console.log("This is a static method ");
+};
+BaseClass.f1();//This is a static method
+var instance1 = new BaseClass();
+instance1.f2();//This is a prototype method
+
+
+//思考下述案例 实例方法 原型方法
+var BaseClass = function() {};
+BaseClass.prototype.method1 = function(){
+    console.log("1 This is a method in Base.prototype");
+};
+var instance1 = new BaseClass();
+instance1.method1();
+
+instance1.method1 = function(){
+    console.log("2 This is a method in instance1");
+};
+instance1.method1();//访问的哪一个method1？
+
+
+// 思考下述实例
+/*
+var BaseClass = function() {
+    this.method1 = function(){
+        console.log('1 Defined by the "this" in the instance method');
+    }
+};
+var instance1 = new BaseClass();
+instance1.method1 = function(){
+    console.log('2 Defined directly in the instance method');
+};
+BaseClass.prototype.method1 = function(){
+    console.log('3 Defined by the prototype instance method ');
+};
+instance1.method1();//Defined directly in the instance method
+*/
+
+//Part 1 constructor属性的应用
+
+// 1 确定对象的构造函数名
+function Foo() {}
+var f = new Foo();
+console.log(f.constructor.name);
+
+// 2 创建相似对象
+function Constr(name) {
+    this.name = name;
+}
+var x = new Constr("Jack");
+var y = new x.constructor("Mike");
+console.log(y);
+console.log(y instanceof Constr);
+
+// 3 constructor可用于指定构造函数
+function Person(area){
+    this.type = 'person';
+    this.area = area;
+}
+Person.prototype.sayArea = function(){
+    console.log(this.area);
+};
+var Father = function(age){
+    this.age = age;
+};
+Father.prototype = new Person('Beijin');
+console.log(Person.prototype.constructor); //function person()
+console.log(Father.prototype.constructor); //function person()
+Father.prototype.constructor = Father;     //修正constructor指向
+console.log(Father.prototype.constructor); //function father()
+var one = new Father(25);
+
+
+//Part2 公有属性、私有属性、特权方法
+function A(id) {
+    this.publicId = id;
+    var privateId = 456;
+    this.getId = function () {
+        console.log(this.publicId,privateId);
+    };
+}
+var a = new A(123);
+console.log(a.publicId);
+// console.log(a.privateId);
+a.getId();
+
+
+//思考哪些是true，哪些是false
+function Person(name){
+	this.name = name;
+}
+Person.prototype.getName = function(){}
+// Person.prototype = { //测试如果更改了Person.prototype输出又会变成什么?
+// 	getName:function(){}
+// }
+var p = new Person("jack");
+console.log(p.__proto__ === Person.prototype);//true or false?
+console.log(p.__proto__ === p.constructor.prototype);//true or false?
+console.log(Object.prototype === p.constructor.prototype);//true or false?
+console.log(({getName:function(){}}).__proto__ === p.constructor.prototype);//true or false?
+
+
+
+//补充：Shape 多态
+
+/*
+//测试
+function A(){
+    this.a = 10;
+    this.af = function(){console.log(this.a);}
+}
+var a = new A();
+
+function B(){
+    A.call(this);
+    this.b = 20;
+    this.bf = function(){console.log(this.a);}
+}
+B.prototype.__proto__ = A.prototype;
+var b = new B();
+*/
