945a8993a2b8e4a79754b66a5685c35c8a2e0995
lixiaofan1997 <773515092@qq.com>
2018-06-24 10:05:15 +0800

    ES6

---
+++ LS31/PPT31.js
@@ -0,0 +1,304 @@
+//PPT31 ES6新增数据类型与数据结构
+
+//Part1111111111111111
+//为什么要使用Symbol
+// ES5里面对象的属性名都是字符串，
+// 如果你需要使用一个别人提供的对象，你对这个对象有哪些属性也不是很清楚，
+// 但又想为这个对象新增一些属性，那么你新增的属性名就很可能和原来的属性名发送冲突
+// 例如：
+var obj = {
+    x:1,
+    y:2,
+    moveTo:function(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
+obj.moveTo = function(x,y){
+    console.log("方法被覆盖了");
+};
+obj.moveTo(0,0);
+// 显然我们是不希望这种情况发生的。所以，我们需要确保每个属性名都是独一无二
+// 因此，ES6里就引入了Symbol，用它来产生一个独一无二的值。
+
+//定义Symbol变量，注意Symbol是基本数据类型的一种，不能用new
+//回顾下基本数据类型的特点，区分基本类型和引用类型
+let s = Symbol();//不能用new
+typeof s;// "symbol"
+
+//Symbol函数可以接受一个字符串作为参数，表示对Symbol实例的描述，主要是为了在控制台显示，或者转为字符串时，比较容易区分。
+var s1 = Symbol('foo');
+var s2 = Symbol('bar');
+console.log(s1); // Symbol(foo)
+console.log(s2); // Symbol(bar)
+console.log(s1.toString()); // "Symbol(foo)"
+console.log(s2.toString()); // "Symbol(bar)"
+
+
+//Part2222222222222222222
+//注意，Symbol函数的参数只是表示对当前Symbol值的描述，因此相同参数的Symbol函数的返回值是不相等的。
+// 没有参数的情况
+var s1 = Symbol();
+var s2 = Symbol();
+s1 === s2 // false
+// 有参数的情况
+var s1 = Symbol("foo");
+var s2 = Symbol("foo");
+s1 === s2 // false
+
+//如果 Symbol 的参数是一个对象，就会调用该对象的toString方法，将其转为字符串，然后才生成一个 Symbol 值
+const obj = {
+    toString() {
+        return 'abc';
+    }
+};
+const sym = Symbol(obj);
+sym // Symbol(abc)
+
+//Symbol值不能与其他类型的值进行运算，会报错。
+var sym = Symbol('My symbol');
+//"your symbol is " + sym;//报错
+//但是，Symbol值可以显式转为字符串。
+var sym = Symbol('My symbol');
+String(sym); // 'Symbol(My symbol)'
+sym.toString(); // 'Symbol(My symbol)'
+
+//Part111111111111111
+//由于每一个Symbol值都是不相等的，这意味着Symbol值可以作为标识符，用于对象的属性名，
+//就能保证不会出现同名的属性。
+//这对于一个对象由多个模块构成的情况非常有用，能防止某一个键被不小心改写或覆盖。
+//使用Symbol是用[]，而不是用点操作符
+var mySymbol = Symbol();
+// 第一种写法
+var a = {};
+a[mySymbol] = 'Hello!';//注意中括号内不要加引号，后面介绍加引号和不加引号的区别
+// 第二种写法
+var a = {
+    [mySymbol]: 'Hello!'
+};
+// 第三种写法
+var a = {};
+Object.defineProperty(a, mySymbol, { value: 'Hello!' });
+// 以上写法都得到同样结果
+a[mySymbol] // "Hello!"
+
+
+
+//思考下述代码，是添加了几个属性，obj对象有几个属性？
+var aSymbol = Symbol("abc");
+var obj = {
+    [aSymbol]: 'Hello!'
+};
+Object.defineProperty(obj, Symbol("abc"), { value: 'World!' });
+console.log(obj);//思考obj对象有几个属性？
+
+//Part22222222222222222
+//上面代码通过方括号结构和Object.defineProperty，将对象的属性名指定为一个Symbol值。
+//注意，Symbol值作为对象属性名时，不能用点运算符，使用中括号是注意使用引号和不用引号的区别
+var mySymbol = Symbol();
+var a = {};
+a.mySymbol = 'Hello!';
+a[mySymbol] // undefined
+a['mySymbol'] // "Hello!"
+//上面代码中，因为点运算符后面总是字符串，所以不会读取mySymbol作为标识名所指代的那个值，导致a的属性名实际上是一个字符串，而不是一个
+//Symbol值。
+
+//思考：
+var myS1 = Symbol("xx");
+var myS2 = "xx";
+var obj = {
+    [myS1]:123,
+    [myS2]:456
+};
+console.log(obj[myS1],obj[Symbol("xx")]);//输出什么？如何解释Symbol
+console.log(obj[myS2],obj["xx"]);
+// console.log(obj["myS1"]);
+// console.log(obj["myS2"]);
+
+//同理，在对象的内部，使用Symbol值定义属性时，Symbol值必须放在方括号之中
+// 如果不用[]的话相当于使用s对应的字符串定义属性
+let s = Symbol();
+let obj = {
+    [s]: function (arg) {console.log("xx");}
+};
+obj[s](123);
+// 上面代码中，如果s不放在方括号中，该属性的键名就是字符串s，而不是s所代表的那个Symbol值。
+
+// 采用增强的对象写法，上面代码的obj对象可以写得更简洁一些
+let obj = {
+    [s](arg) {console.log("xx");}
+};
+
+// 回顾ES6对象属性的表达式定义方法和ES6对象的简洁表示法，对于属性和方法定义的简洁表示法
+//还有一点需要注意，Symbol值作为属性名时，该属性还是公开属性，不是私有属性
+
+//Part111111111111111111111111
+//遍历实例一
+var obj = {};
+var a = Symbol('a');
+var b = Symbol('b');
+obj[a] = 'Hello';
+obj[b] = 'World';
+var objectSymbols = Object.getOwnPropertySymbols(obj);
+console.log(objectSymbols);// [Symbol(a), Symbol(b)]
+
+//遍历实例二
+var obj = {};
+var foo = Symbol("foo");
+Object.defineProperty(obj, foo, {
+    value: "foo bar",
+});
+for (var i in obj) {
+    console.log(i); // 无输出
+}
+Object.getOwnPropertyNames(obj);// []
+Object.getOwnPropertySymbols(obj);// [Symbol(foo)]
+
+
+//Part2222222222222222222222
+var s1 = Symbol.for('foo');
+var s2 = Symbol.for('foo');
+console.log(s1 === s2); // true
+
+//Symbol.for()与Symbol()这两种写法，都会生成新的Symbol。
+// 它们的区别是，前者会被登记在全局环境中供搜索，后者不会。
+// Symbol.for()不会每次调用就返回一个新的Symbol类型的值，而是会先检查给定的key是否已经存在，
+// 如果不存在才会新建一个值。比如，如果你调用Symbol.for("cat")30次，每次都会返回同一个Symbol值，
+// 但是调用Symbol("cat")30次，会返回30个不同的Symbol值。
+
+console.log(Symbol.for("bar") === Symbol.for("bar"));// true
+console.log(Symbol("bar") === Symbol("bar"));// false
+console.log(Symbol.for("bar") === Symbol("bar"));// false
+
+//Symbol.keyFor方法返回一个已登记的Symbol类型值的key。
+var s1 = Symbol.for("foo");
+console.log(Symbol.keyFor(s1)); // "foo"
+var s2 = Symbol("foo");
+console.log(Symbol.keyFor(s2)); // undefined
+
+//思考：
+var s3 = Symbol(Symbol.keyFor(s1));
+console.log(s1 === s3);
+console.log(s2 === s3);
+var s4 = Symbol.for(Symbol.keyFor(s1));
+console.log(s1 === s4);
+console.log(s2 === s4);
+
+
+
+//Part 111111111111111111111111111111111111111
+var s1 = new Set([1,2,3,4,5,5,6,2,2]);
+console.log(s1);
+
+var s2 = new Set();
+[2, 3, 5, 4, 5, 2, 2].map(x => s2.add(x));
+for (var i of s2) {
+    console.log(i);
+}// 2 3 5 4
+
+//
+// 例一
+var set = new Set([1, 2, 3, 4, 4]);
+console.log([...set]);
+// [1, 2, 3, 4]
+// 例二
+var items = new Set([1, 2, 3, 4, 5, 5, 5, 5]);
+console.log(items.size); // 5
+
+// 去除数组的重复成员
+[...new Set([1,2,3,3])];
+
+//
+//区分基本类型和引用（对象）类型，两个对象总是不相等的，思考下述代码
+var set = new Set();
+set.add({});
+console.log(set.size); // 1
+set.add({});
+console.log(set.size); // 2
+
+
+//Part 22222222222222222222222222222222222222222222222
+s.add(1).add(2).add(2);
+// 注意2被加入了两次
+s.size // 2
+s.has(1); // true
+s.has(2); // true
+s.has(3); // false
+s.delete(2);
+s.has(2); // false
+
+//
+var properties = new Set();
+properties.add('width');
+properties.add('height');
+console.log(properties.size);
+if (properties.has('width')&&properties.has('height')) {
+    console.log("do something!");
+}
+
+//Array.from方法可以将Set结构转为数组。
+var items = new Set([1, 2, 3, 4, 5]);
+var array = Array.from(items);
+
+//...也可以将set转换成数组
+console.log([...(new Set([1, 2, 3, 4, 5]))]);//[1, 2, 3, 4, 5]
+//如果不加[]就是散列的元素
+console.log(...(new Set([1, 2, 3, 4, 5])));//1, 2, 3, 4, 5
+
+
+//关于Set的遍历方法
+var set = new Set(['red', 'green', 'blue']);
+console.log(typeof set.keys());//注意是什么类型，是否可迭代，是否可用for...of遍历
+console.log(typeof set.values());
+console.log(typeof set.entries());
+
+//keys方法、values方法、entries方法返回的都是遍历器对象
+for (var item of set.keys()) {
+    console.log(item);
+}
+// red
+// green
+// blue
+for (var item of set.values()) {
+    console.log(item);
+}
+// red
+// green
+// blue
+for (var item of set.entries()) {
+    console.log(item);
+}
+// ["red", "red"]
+// ["green", "green"]
+// ["blue", "blue"]
+
+//练习：使用解构赋值，将数据提取
+for (var [key,value] of set.entries()) {
+    console.log(key,value);
+}
+
+//Set结构的实例的forEach方法，用于对每个成员执行某种操作，没有返回值。
+var set = new Set([1, 2, 3]);
+set.forEach((value, key) => console.log(value * 2) );
+// 2
+// 4
+// 6
+
+//而且，数组的map和filter方法也可以间接用于Set了，通过...转成数组后调用后再生成set
+var set = new Set([1, 2, 3]);
+set = new Set([...set].map(x => x * 2));
+// 返回Set结构：{2, 4, 6}
+var set = new Set([1, 2, 3, 4, 5]);
+set = new Set([...set].filter(x => (x % 2) == 0));
+// 返回Set结构：{2, 4}
+
+
+// set应用案例 并集、交集
+let a = new Set([1, 2, 3]);
+let b = new Set([4, 3, 2]);
+// 并集
+let union = new Set([...a, ...b]);
+// Set {1, 2, 3, 4}
+// 交集
+let intersect = new Set([...a].filter(x => b.has(x)));
+// set {2, 3}
